fe649383cc6c233c6736aa2a6b808a20809a0963
StavBl <blstav@gmail.com>
2017-07-22 17:32:18 +0300

    more design

--- app/relabel/src/app/login/login.component.ts
+++ app/relabel/src/app/login/login.component.ts
@@ -13,8 +13,9 @@ import { User } from "../models/user";
 export class LoginComponent implements OnInit {
     user: User;
     model: any = {};
-    username: string = 'aaabbb';
-    password: string = 'bbbbbb';
+    username: string = '';
+    password: string = '';
+    notValid: boolean = false;
 
   constructor(private loginservice: LoginService,
               public router: Router,
@@ -24,35 +25,21 @@ export class LoginComponent implements OnInit {
       this.loginservice.logout();
   }
   login(form:NgForm){
-      const val = form.value;
-      console.log(val);   
-      this.loginservice.login(val.username,val.password).then((usr) => {
-       if (usr && usr.token) {
-            // store user details and jwt token in local storage to keep user logged in between page refreshes
-            localStorage.setItem('currentUser', JSON.stringify(usr));
-            this.user = JSON.parse(localStorage.getItem('currentUser'));
-            console.log(this.user.username);
-            this.loginservice.currentUser.emit(this.user);
-            this.router.navigate(['./dashboard'])
-            }
-        else{
-          console.log("wrong user name or password");
-          this.alertService.error("wrong user name or password");
-        }
-      
+    const val = form.value;
+    console.log(val);   
+    this.loginservice.login(val.username,val.password).then((usr) => {
+      if (usr && usr.token) {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify(usr));
+          this.user = JSON.parse(localStorage.getItem('currentUser'));
+          console.log(this.user.username);
+          this.loginservice.currentUser.emit(this.user);
+          this.router.navigate(['./dashboard'])
+          }
+      else{
+        console.log("wrong user name or password");
+        this.alertService.error("wrong user name or password");
+      }
     }); 
   }
-  submit() {
-    
-  }
-  // isLogedIn() {
-  //   if(this.user && this.user.token)
-  //     return true;
-  //   var usr = localStorage.getItem('currentUser');
-  //   if(usr !== null) {
-  //     this.user = JSON.parse(usr)
-  //     return true;
-  //   }
-  //   return false;
-  // }
 }
